91cfa323b9d6956c33045a58564d543608dc6f5e
Javier Reyes <javier.reys@gmail.com>
2021-12-03 17:34:49 -0300

    refactor: :lipstick: update styles of the page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { teal } = require('tailwindcss/colors')
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
     mode: 'jit',
@@ -9,6 +10,11 @@ module.exports = {
         extend: {
             colors: {
                 teal,
+                'very-dark-blue': 'hsl(207, 26%, 17%)',
+                'dark-blue': 'hsl(209, 23%, 22%)',
+            },
+            fontFamily: {
+                sans: ['Nunito Sans', ...fontFamily.sans],
             },
         },
     },
