82411705c58bbffcacd19553c4c80fed0d22f9b2
Javier Reyes <javier.reys@gmail.com>
2021-12-03 18:32:44 -0300

    feat: :sparkles: add navbar to the entire app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
 <template>
+    <TheNavbar />
     <RouterView />
 </template>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="shadow-md">
+        <div
+            class="px-2 mx-auto shadow-md max-w-7xl sm:px-6 lg:px-8 bg-dark-blue"
+        >
+            <div class="relative flex items-center justify-between h-16">
+                <div class="text-lg font-extrabold tracking-wide">
+                    Where in the world?
+                </div>
+                <div class="flex space-x-4">
+                    <div class="cursor-pointer hover:text-gray-500">
+                        <MoonIcon class="w-5 h-5" />
+                    </div>
+                    <div class="cursor-pointer hover:text-gray-500">
+                        <TranslateIcon class="w-5 h-5" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { MoonIcon, TranslateIcon } from '@heroicons/vue/solid'
+</script>
