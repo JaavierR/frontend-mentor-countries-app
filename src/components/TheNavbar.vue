739336afe7a0b8b81a232233a8bbcf34a759ea4e
Javier Reyes <javier.reys@gmail.com>
2021-12-04 00:51:43 -0300

    feat: :globe_with_meridians: implement basic i18n in the navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import { MoonIcon, TranslateIcon } from '@heroicons/vue/solid'
+const { t, availableLocales, locale } = useI18n()
+
+const toggleLocales = () => {
+    const locales = availableLocales
+    locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
+
 <template>
     <div
         class="fixed top-0 z-50 w-full bg-opacity-75 shadow-md bg-dark-blue backdrop-filter backdrop-blur-md"
@@ -7,15 +17,22 @@
                 <div class="relative flex items-center justify-between h-16">
                     <RouterLink
                         :to="{ name: 'Home' }"
-                        class="text-lg font-extrabold tracking-wide"
+                        class="text-xl font-extrabold tracking-wide"
                     >
-                        Where in the world?
+                        {{ t('navbar.title') }}
                     </RouterLink>
                     <div class="flex space-x-4">
-                        <div class="cursor-pointer hover:text-gray-500">
+                        <div
+                            :title="t('navbar.toggle_dark')"
+                            class="cursor-pointer hover:text-teal-500"
+                        >
                             <MoonIcon class="w-5 h-5" />
                         </div>
-                        <div class="cursor-pointer hover:text-gray-500">
+                        <div
+                            class="cursor-pointer hover:text-teal-500"
+                            :title="t('navbar.toggle_langs')"
+                            @click.prevent="toggleLocales"
+                        >
                             <TranslateIcon class="w-5 h-5" />
                         </div>
                     </div>
@@ -24,7 +41,3 @@
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-import { MoonIcon, TranslateIcon } from '@heroicons/vue/solid'
-</script>
